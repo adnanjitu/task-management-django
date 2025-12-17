531d836f041771078d2a7cb8d688fd4031ec31cd
Adnan Jitu <adnannihal61@gmail.com>
2025-12-17 13:33:46 +0600

    Initial project setup done

---
+++ task_management/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from tasks.views import home, contact, about
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home),
+    path('contact/', contact),
+    path('about/', about),
+    path("tasks/", include("tasks.urls"))
+]
